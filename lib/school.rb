168c745c01153ed24fd36ee57c7b588cb4eefa6f
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-07 21:32:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name 
+     @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+
+ @roster[grade] = []
+ @roster[grade] << name
+  
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+    
+  end
